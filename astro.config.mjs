eca043077d7ac413fbb803ceae50ac9d2b30e0a1
Danny Guo <dannyguo91@gmail.com>
2022-08-27 13:04:14 -0400

    Add a sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
     site: "https://www.dannyguo.com",
+    integrations: [sitemap()],
 });

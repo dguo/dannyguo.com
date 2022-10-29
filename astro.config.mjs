34f025398052a1480a046a7a89cdaa9d1bd0e396
Danny Guo <dannyguo91@gmail.com>
2022-10-29 13:05:59 -0400

    Implement the word count and estimated reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,21 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import { toString } from "mdast-util-to-string";
+import getReadingTime from "reading-time";
+
+export function remarkReadingTime() {
+    return function (tree, { data }) {
+        const textOnPage = toString(tree);
+        const readingTime = getReadingTime(textOnPage);
+        data.astro.frontmatter.minutesToRead = Math.ceil(readingTime.minutes);
+        data.astro.frontmatter.numberOfWords = readingTime.words;
+    };
+}
 
 export default defineConfig({
     site: "https://www.dannyguo.com",
     integrations: [sitemap()],
+    markdown: {
+        remarkPlugins: [remarkReadingTime],
+    },
 });

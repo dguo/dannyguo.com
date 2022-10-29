9d07b4e4c32e8d91ee15d92e51b7c0ff64a12918
Danny Guo <dannyguo91@gmail.com>
2022-10-29 13:34:48 -0400

    Set up Prettier

--- public/js/copy-code-button.js
+++ public/js/copy-code-button.js
@@ -2,33 +2,33 @@
 /* eslint-disable no-console */
 
 function addCopyButtons(clipboard) {
-    document.querySelectorAll('pre > code').forEach(function(codeBlock) {
-        var button = document.createElement('button');
-        button.className = 'copy-code-button';
-        button.type = 'button';
-        button.innerText = 'Copy';
+    document.querySelectorAll("pre > code").forEach(function (codeBlock) {
+        var button = document.createElement("button");
+        button.className = "copy-code-button";
+        button.type = "button";
+        button.innerText = "Copy";
 
-        button.addEventListener('click', function() {
+        button.addEventListener("click", function () {
             clipboard.writeText(codeBlock.textContent).then(
-                function() {
+                function () {
                     /* Chrome doesn't seem to blur automatically, leaving the button
                        in a focused state */
                     button.blur();
 
-                    button.innerText = 'Copied!';
-                    setTimeout(function() {
-                        button.innerText = 'Copy';
+                    button.innerText = "Copied!";
+                    setTimeout(function () {
+                        button.innerText = "Copy";
                     }, 2000);
                 },
-                function(error) {
-                    button.innerText = 'Error';
+                function (error) {
+                    button.innerText = "Error";
                     console.error(error);
                 }
             );
         });
 
         var pre = codeBlock.parentNode;
-        if (pre.parentNode.classList.contains('highlight')) {
+        if (pre.parentNode.classList.contains("highlight")) {
             var highlight = pre.parentNode;
             highlight.parentNode.insertBefore(button, highlight);
         } else {
@@ -40,13 +40,13 @@ function addCopyButtons(clipboard) {
 if (navigator && navigator.clipboard) {
     addCopyButtons(navigator.clipboard);
 } else {
-    var script = document.createElement('script');
+    var script = document.createElement("script");
     script.src =
-        'https://cdnjs.cloudflare.com/ajax/libs/clipboard-polyfill/2.7.0/clipboard-polyfill.promise.js';
-    script.integrity = 'sha256-waClS2re9NUbXRsryKoof+F9qc1gjjIhc2eT7ZbIv94=';
-    script.crossOrigin = 'anonymous';
+        "https://cdnjs.cloudflare.com/ajax/libs/clipboard-polyfill/2.7.0/clipboard-polyfill.promise.js";
+    script.integrity = "sha256-waClS2re9NUbXRsryKoof+F9qc1gjjIhc2eT7ZbIv94=";
+    script.crossOrigin = "anonymous";
 
-    script.onload = function() {
+    script.onload = function () {
         addCopyButtons(clipboard);
     };
 

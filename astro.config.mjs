7179281a80e401964eca81a653525e0e04c5928d
Danny Guo <dannyguo91@gmail.com>
2022-11-10 23:41:24 -0500

    Add anchor links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import { h } from "hastscript";
 import { toString } from "mdast-util-to-string";
 import getReadingTime from "reading-time";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 
 export function remarkReadingTime() {
     return function (tree, { data }) {
@@ -18,6 +21,26 @@ export default defineConfig({
     integrations: [mdx(), sitemap()],
     markdown: {
         remarkPlugins: [remarkReadingTime],
+        rehypePlugins: [
+            rehypeSlug,
+            [
+                rehypeAutolinkHeadings,
+                {
+                    behavior: "append",
+                    properties: { class: "anchor" },
+                    content: (node) => {
+                        return [
+                            h("svg.icon.icon-link", { ariaHidden: "true" }, [
+                                h("use", {
+                                    href: "/images/icons.svg#icon-link",
+                                }),
+                            ]),
+                        ];
+                    },
+                    test: ["h2", "h3", "h4", "h5", "h6"],
+                },
+            ],
+        ],
         shikiConfig: {
             theme: "monokai",
         },
